a851d4820cd5ae1d3ef6df212bfd03868f7fde3c
Nathaniel Rivera <riven18@farmingdale.edu>
2025-06-19 15:50:48 -0400

    Created some parts of the landing screen such as the pfp selector

--- src/main/java/com/example/csc325_capstoneproject/StudyController.java
+++ src/main/java/com/example/csc325_capstoneproject/StudyController.java
@@ -2,13 +2,36 @@ package com.example.csc325_capstoneproject;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+
+/**
+ * Controller class which controls the actions for the landing page of the application.
+ * @since 6/14/2025
+ * @author Nathaniel Rivera
+ */
 public class StudyController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected ImageView pfp;
+
+    /**
+     * Allows the user to change their profile picture.
+     * @since 6/19/2025
+     * @author Nathaniel Rivera
+     */
+    @FXML
+    protected void changePFP() {
+
+        File file = (new FileChooser()).showOpenDialog(pfp.getScene().getWindow());
+
+        if(file != null) {
+            pfp.setImage(new Image(file.toURI().toString()));
+        }
+
     }
+
 }
