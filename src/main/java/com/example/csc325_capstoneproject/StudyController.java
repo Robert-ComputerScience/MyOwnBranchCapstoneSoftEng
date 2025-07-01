484097fc0a257f10905d7af3fbd1fb2e3775ed56
Nathaniel Rivera <riven18@farmingdale.edu>
2025-07-01 19:38:00 -0400

    theme swapper for the science and history buttons implemented

--- src/main/java/com/example/csc325_capstoneproject/StudyController.java
+++ src/main/java/com/example/csc325_capstoneproject/StudyController.java
@@ -41,6 +41,12 @@ public class StudyController implements Initializable {
     protected Button englishButton;
 
     @FXML
+    protected Button historyButton;
+
+    @FXML
+    protected Button scienceButton;
+
+    @FXML
     protected TableView<Test> tv;
 
     @FXML
@@ -57,6 +63,12 @@ public class StudyController implements Initializable {
     protected int currentGradeLevel;
 
     protected ObservableList<Test> math_tests = FXCollections.observableList(new LinkedList<>());
+
+    protected ObservableList<Test> english_tests = FXCollections.observableList(new LinkedList<>());
+
+    protected ObservableList<Test> history_tests = FXCollections.observableList(new LinkedList<>());
+
+    protected ObservableList<Test> science_tests = FXCollections.observableList(new LinkedList<>());
 
     /**
      * Initializes the TableView and Percentage wheel on the main page. The default is math.
@@ -89,14 +101,13 @@ public class StudyController implements Initializable {
         currentSubject = Subject.math;
 
         try {
+
             Scene scene = mathButton.getScene();
             Stage stage = (Stage) scene.getWindow();
             stage.getScene().getStylesheets().clear();
             scene.getStylesheets().add(getClass().getResource("math-theme.css").toExternalForm());
             stage.setScene(scene);
             stage.show();
-
-            System.out.println("math " + scene.getStylesheets());
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -110,10 +121,11 @@ public class StudyController implements Initializable {
      */
     @FXML
     protected void englishMode() {
-        tv.setItems(math_tests);
+        tv.setItems(english_tests);
         currentSubject = Subject.english;
 
         try {
+
             Scene scene = englishButton.getScene();
             Stage stage = (Stage) scene.getWindow();
             stage.getScene().getStylesheets().clear();
@@ -121,7 +133,53 @@ public class StudyController implements Initializable {
             stage.setScene(scene);
             stage.show();
 
-            System.out.println("english " + scene.getStylesheets());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Switches the page into history mode changing the stylesheets and the average and the list of the past tests.
+     * @since 7/01/2025
+     * @author Nathaniel Rivera
+     */
+    @FXML
+    protected void historyMode() {
+        tv.setItems(history_tests);
+        currentSubject = Subject.history;
+
+        try {
+            Scene scene = historyButton.getScene();
+            Stage stage = (Stage) scene.getWindow();
+            stage.getScene().getStylesheets().clear();
+            scene.getStylesheets().add(getClass().getResource("history-theme.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Switches the page into science mode changing the stylesheets and the average and the list of the past tests.
+     * @since 7/01/2025
+     * @author Nathaniel Rivera
+     */
+    @FXML
+    protected void scienceMode() {
+        tv.setItems(science_tests);
+        currentSubject = Subject.science;
+
+        try {
+            Scene scene = scienceButton.getScene();
+            Stage stage = (Stage) scene.getWindow();
+            stage.getScene().getStylesheets().clear();
+            scene.getStylesheets().add(getClass().getResource("science-theme.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
 
         } catch (Exception e) {
             e.printStackTrace();
